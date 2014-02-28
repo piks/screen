d38651f789e4e4856d9bca864f5f4091ec0ed989
pika <ladymax24@aol.com>
2014-02-28 09:31:57 -0500

    added part of strip

--- screen.py
+++ screen.py
@@ -1,4 +1,4 @@
-def screen(mode="html",full="",start="",stop="",instance=1):
+def screen(mode="html",full="",start="",stop="",instance=0):
 	if mode=="html":
 		start,stop="<",">"
 		while start in full:
@@ -6,3 +6,11 @@ def screen(mode="html",full="",start="",stop="",instance=1):
 			b=full.find(stop)
 			full=full.replace(full[a:b+1],"")
 		return full
+	elif mode=="strip":
+		print(instance)
+		if instance=="all":
+			while start in full:
+				a=full.find(start)
+				b=full.find(stop)
+				full=full.replace(full[a:b+1],"")	
+				print(full)
